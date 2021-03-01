873d46bd3cbdbe08d33acf112ca513a223440d25
Miguel Lopez <mlopez@vizioz.com>
2021-03-01 16:07:47 +0000

    show list value with categories

---
+++ CovidSupport.Api/Models/CategoryResources.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CovidSupport.Api.Models
+{
+    public class CategoryResources
+    {
+        public IEnumerable<IResourceItemBase> Markers { get; set; }
+
+        public string HighlightFilters { get; set; }
+
+        public bool ShowListOnly { get; set; }
+    }
+}
